e1977179baa21fbaa104add949671e315c2a5c61
15138730370 <1037483576@qq.com>
2017-11-04 07:11:32 +0800

    save

--- src/app/design/design.module.ts
+++ src/app/design/design.module.ts
@@ -2,7 +2,6 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { Components } from './components';
-
 import { FreeWidgetComponent } from './layouts';
 
 @NgModule({
